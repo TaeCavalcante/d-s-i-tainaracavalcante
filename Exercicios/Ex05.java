d9659c8a81cc7642d8e454686553e31c6b378050
TaeCavalcante <110575732+TaeCavalcante@users.noreply.github.com>
2023-03-22 00:16:42 -0300

    Create Ex05.java
    Exercício 5

--- Exercicios/Ex05.java
+++ Exercicios/Ex05.java
@@ -10,4 +10,18 @@ package Exercicios;
  */
 public class Ex05 {
     
+public  static  void  principal ( String [] args ){
+         Scanner  entrada = new  Scanner ( System . in );
+         
+         int  base , altura ;
+         
+           Sistema . fora . println ( "Digite o valor da altura: " );
+           altura = entrada . proximoInt ();
+           
+           Sistema . fora . println ( "Digite o valor da base: " );
+           base = entrada . proximoInt ();
+           
+           int  resultado = base * altura / 2 ;
+            Sistema . fora . println ( "O valor da área do triângulo é: " + resultado );
+
 }
